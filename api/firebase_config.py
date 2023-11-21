867159a3670d5846194c12019f64a45a4e9fdcb7
hpekkan <hpekkan@etu.edu.tr>
2023-11-21 19:53:47 +0300

    bug fix:requirments.txt

--- api/firebase_config.py
+++ api/firebase_config.py
@@ -1,4 +1,11 @@
 import firebase_admin
+import os
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+json_file = ""
+for f in files:
+    if f.startswith("gezbot"):
+        json_file = f
 
-cred = firebase_admin.credentials.Certificate('./gezbot-112ee-firebase-adminsdk-2ul3v-5240d2f6ae.json')
+
+cred = firebase_admin.credentials.Certificate(json_file)
 firebase_admin.initialize_app(cred)
