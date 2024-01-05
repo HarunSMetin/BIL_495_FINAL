10c54e25e06d10a28f1bc025ecf50081766f0864
hpekkan <hpekkan@etu.edu.tr>
2024-01-05 18:26:42 +0300

    merged styles with backend

---
+++ gezbot/lib/pages/login_screen/components/login_content.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:gezbot/utils/helper_functions.dart';
+
+import 'package:gezbot/shared/constants.dart';
+import '../animations/change_screen_animation.dart';
+import 'bottom_text.dart';
+import 'top_text.dart';
+
+import 'package:gezbot/services/user_service.dart';
+
+enum Screens {
+  createAccount,
+  welcomeBack,
+}
+
+class LoginContent extends StatefulWidget {
+  const LoginContent({Key? key}) : super(key: key);
+
+  @override
+  State<LoginContent> createState() => _LoginContentState();
+}
+
+class _LoginContentState extends State<LoginContent>
+    with TickerProviderStateMixin {
+  late final List<Widget> createAccountContent;
+  late final List<Widget> loginContent;
+  final _userService = UserService();
+
+  void login_or_register(
+    BuildContext context,
+    void Function(String) _showErrorDialog,
+  ) async {
+    if (ChangeScreenAnimation.currentScreen == Screens.createAccount) {
+      try {
+        await _userService.signInWithGoogle(
+          context: context,
+          showErrorDialog: _showErrorDialog,
+        );
+      } catch (e) {
+        _showErrorDialog('An error occurred. Please try again.');
+      }
+    } else {
+      try {
+        await _userService.signInWithGoogle(
+          context: context,
+          showErrorDialog: _showErrorDialog,
+        );
+      } catch (e) {
+        _showErrorDialog('An error occurred. Please try again.');
+      }
+    }
+  }
+
+  Widget inputField(String hint, IconData iconData) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 8),
+      child: SizedBox(
+        height: 50,
+        child: Material(
+          elevation: 8,
+          shadowColor: Colors.black87,
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(30),
+          child: TextField(
+            textAlignVertical: TextAlignVertical.bottom,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide.none,
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              hintText: hint,
+              prefixIcon: Icon(iconData),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget loginButton(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 75, vertical: 16),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 14),
+          shape: const StadiumBorder(),
+          primary: kSecondaryColor,
+          elevation: 8,
+          shadowColor: Colors.black87,
+        ),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget orDivider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 130, vertical: 8),
+      child: Row(
+        children: [
+          Flexible(
+            child: Container(
+              height: 1,
+              color: kPrimaryColor,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              'or',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Flexible(
+            child: Container(
+              height: 1,
+              color: kPrimaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget logos() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            void _showErrorDialog(String message) {
+              showDialog(
+                context: context,
+                builder: (ctx) => AlertDialog(
+                  title: const Text('An error occurred'),
+                  content: Text(message),
+                  actions: <Widget>[
+                    TextButton(
+                      child: const Text('Okay'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop();
+                      },
+                    )
+                  ],
+                ),
+              );
+            }
+
+            login_or_register(context, _showErrorDialog);
+          },
+          child: Image.asset('assets/images/google.png'),
+        ),
+        //Image.asset('assets/images/google.png'),
+      ),
+    );
+  }
+
+  Widget forgotPassword() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 75),
+      child: TextButton(
+        onPressed: () {},
+        child: const Text(
+          'Forgot Password?',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: kSecondaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    createAccountContent = [
+      inputField('Name', Ionicons.person_outline),
+      inputField('Email', Ionicons.mail_outline),
+      inputField('Password', Ionicons.lock_closed_outline),
+      loginButton('Sign Up'),
+      orDivider(),
+      logos(),
+    ];
+
+    loginContent = [
+      inputField('Email', Ionicons.mail_outline),
+      inputField('Password', Ionicons.lock_closed_outline),
+      loginButton('Log In'),
+      forgotPassword(),
+      logos(),
+    ];
+
+    ChangeScreenAnimation.initialize(
+      vsync: this,
+      createAccountItems: createAccountContent.length,
+      loginItems: loginContent.length,
+    );
+
+    for (var i = 0; i < createAccountContent.length; i++) {
+      createAccountContent[i] = HelperFunctions.wrapWithAnimatedBuilder(
+        animation: ChangeScreenAnimation.createAccountAnimations[i],
+        child: createAccountContent[i],
+      );
+    }
+
+    for (var i = 0; i < loginContent.length; i++) {
+      loginContent[i] = HelperFunctions.wrapWithAnimatedBuilder(
+        animation: ChangeScreenAnimation.loginAnimations[i],
+        child: loginContent[i],
+      );
+    }
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    ChangeScreenAnimation.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const Positioned(
+          top: 136,
+          left: 24,
+          child: TopText(),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: createAccountContent,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: loginContent,
+              ),
+            ],
+          ),
+        ),
+        const Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 50),
+            child: BottomText(),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- gezbot/lib/shared/constants.dart
+++ gezbot/lib/shared/constants.dart
@@ -9,3 +9,8 @@ const TextInputDecoration = InputDecoration(
         borderSide: BorderSide(color: Colors.pink, width: 2.0)));
 
 //.copyWith(hintText: 'Email', prefixIcon: Icon(Icons.email))
+
+// Colors
+const kBackgroundColor = Color(0xFFD2FFF4);
+const kPrimaryColor = Color(0xFF2D5D70);
+const kSecondaryColor = Color(0xFF265DAB);
