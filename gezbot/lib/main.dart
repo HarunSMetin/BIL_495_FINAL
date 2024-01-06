01361e08c437fba777b13fa94e5040b778abba0e
hpekkan <hpekkan@etu.edu.tr>
2024-01-06 14:26:43 +0300

    Fix login screen animation by refining dispose logic; monitor for efficiency impact

--- gezbot/lib/main.dart
+++ gezbot/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'pages/register.dart';
 import 'pages/login_screen/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'pages/homepage.dart';
-import 'dart:math' as math;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,62 +18,20 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  Widget topWidget(double screenWidth) {
-    return Transform.rotate(
-      angle: -35 * math.pi / 180,
-      child: Container(
-        width: 1.2 * screenWidth,
-        height: 1.2 * screenWidth,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(150),
-          gradient: const LinearGradient(
-            begin: Alignment(-0.2, -0.8),
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0x007CBFCF),
-              Color(0xB316BFC4),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget bottomWidget(double screenWidth) {
-    return Container(
-      width: 1.5 * screenWidth,
-      height: 1.5 * screenWidth,
-      decoration: const BoxDecoration(
-        shape: BoxShape.circle,
-        gradient: LinearGradient(
-          begin: Alignment(0.6, -1.1),
-          end: Alignment(0.7, 0.8),
-          colors: [
-            Color(0xDB4BE8CC),
-            Color(0x005CDBCF),
-          ],
-        ),
-      ),
-    );
-  }
-
   final bool isLoggedIn;
 
   MyApp({required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: isLoggedIn ? HomePage() : RegisterScreen(),
+      home: isLoggedIn ? HomePage() : LoginScreen(),
       routes: {
         '/login': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
         '/home': (context) => HomePage(),
       },
     );
