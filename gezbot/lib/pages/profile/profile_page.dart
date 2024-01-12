7b2aeb31ddc11b4fd6bd5c22e7b6692b8f37b8bd
ardaeerol <ardaburada@gmail.com>
2024-01-12 21:56:33 +0300

    profile added

--- gezbot/lib/pages/profile/profile_page.dart
+++ gezbot/lib/pages/profile/profile_page.dart
@@ -7,7 +7,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:gezbot/services/user_service.dart'; // Import UserService
 
-//hello
 //from profile branch
 
 class ProfilePage extends StatefulWidget {
