d018780fb1ff8d412fceaf067fbd34640ae64a54
demirrana <demir.rana.bo@gmail.com>
2024-01-22 19:26:34 +0300

    base of home page

---
+++ gezbot/lib/pages/home/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+import 'package:gezbot/services/database_service.dart';
+import 'package:gezbot/services/user_service.dart';
+
+/*
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+*/
