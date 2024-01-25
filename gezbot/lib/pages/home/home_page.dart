22cee1f08254de0c3525325e3526b091067ee12c
demirrana <demir.rana.bo@gmail.com>
2024-01-25 17:10:10 +0300

    Logo is added to pubspec.yaml

--- gezbot/lib/pages/home/home_page.dart
+++ gezbot/lib/pages/home/home_page.dart
@@ -3,13 +3,3 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:gezbot/services/database_service.dart';
 import 'package:gezbot/services/user_service.dart';
-
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState {
-
-}
