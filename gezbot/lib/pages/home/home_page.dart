8598ff104bbce48e156b522f8acf47a2b77cf471
demirrana <demir.rana.bo@gmail.com>
2024-01-25 17:04:31 +0300

    Application logo is added

--- gezbot/lib/pages/home/home_page.dart
+++ gezbot/lib/pages/home/home_page.dart
@@ -4,9 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:gezbot/services/database_service.dart';
 import 'package:gezbot/services/user_service.dart';
 
-/*
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
-*/
+
+class _HomePageState {
+
+}
