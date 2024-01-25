95de9da397a08790497b7470cb665a43d33fc330
demirrana <demir.rana.bo@gmail.com>
2024-01-25 18:20:41 +0300

    A part describing the application is added

--- gezbot/lib/pages/home/home_page.dart
+++ gezbot/lib/pages/home/home_page.dart
@@ -1,5 +1,68 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter/material.dart';
 import 'package:gezbot/services/database_service.dart';
 import 'package:gezbot/services/user_service.dart';
+
+//delete after done
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              AppInfo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AppInfo extends StatefulWidget {
+  @override
+  _AppInfoState createState() => _AppInfoState();
+}
+
+class _AppInfoState extends State<AppInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF3366FF), Color(0xFF00CCFF)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 40.0,
+            height: 40.0,
+            margin: EdgeInsets.only(right: 16.0),
+            child: Image.asset('assets/appicon.jpeg', fit: BoxFit.cover),
+          ),
+          Expanded(
+            child: Text(
+              'Gezbot, gives you the opportunity to socialize while traveling, to ask any inquiries of yours to the chatbot which is up to date.',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
