aa625ded6041d7262fd41327d9c4f0de57dcfda9
Harun Serkan Metin <harun.serkan.metin@gmail.com>
2024-03-10 16:40:19 +0300

    Progress Commit

--- api/gptapi.py
+++ api/gptapi.py
@@ -1,5 +1,28 @@
 import requests
 import json
+import os
+
+exampleInput = {
+    "departureDate": "2024-01-03 00:00:00.000",
+    "returnDate": "2024-01-04 00:00:00.000",
+    "specialComment": "I love eating Pizza. Explore new places and see lots of places",
+    "departureLocation": "Ankara",
+    "desiredDestination": "Istanbul, Söğütlüçeşme",
+    "travelTransportation": "Car",
+    "purposeOfVisit": "Cultural experiences and sightseeing",
+    "estimatedBudget": 5,
+    "accommodationPreferences": "Mid-range",
+    "activitiesPreferences": "Relaxation and wellness (e.g., spas, beaches)",
+    "dietaryRestrictions": "No restrictions",
+    "travelingWithOthers": "Small group (3-5 people)","localRecommendations": "Yes"
+    }
+
+output = {
+    "suggestions": [
+        {"place_types": 
+            {"food_and_drink_based": ["pizza_restaurant"],"entertainment_and_recreation_center": ["tourist_attraction"]},"fields": {"atmospheric_data_fields": ["user_ratings_total"]},"query": "Pizza Restaurants in Istanbul, Söğütlüçeşme"},{"place_types": {"culture_based": ["art_gallery", "museum", "performing_arts_theater"],"entertainment_and_recreation_center": ["amusement_center", "historical_landmark", "tourist_attraction"]},"query": "Historical places in Istanbul"},
+        ]
+    }
 
 traveldata = {
     "id": "PsSAcQDOSL4g5b6q7O7FN",
@@ -26,10 +49,19 @@ traveldata = {
     "lastUpdatedQuestionId": "12_LocalRecommendations",
 }
 
+PROMPT = "#Description:#You're developing an application to help users plan customized travel itineraries using the Google Maps API. Your task is to process structured JSON input, representing a user's travel details and preferences, and generate a comprehensive list of suggested places to visit and activities to engage in during the specified travel period.#Input:#The input comprises a JSON object with the following fields:- `departureDate`: Date of departure.- `returnDate`: Date of return.- `specialComment`: Additional comments or preferences provided by the user.- Additional fields for specific preferences or constraints.#Example Input:#```json{}```#Process:#1. Calculate the travel period duration from `departureDate` to `returnDate`.2. Consider `specialComment` to understand user preferences.3. Generate suggestions proportional to the travel duration, ensuring 10 suggestions in the list.4. Select destinations based on predefined tags like parks, restaurants, etc.5. Customize suggestions with relevant tags and query strings.#Output:#A JSON object with a curated list of suggestions, each containing:- Tags categorizing the suggested place/activity.- Query strings for Google Maps API.#Example Output:#{}#Constraints:#- Ensure diverse suggestions catering to various interests.- Output adheres to specified structure with relevant tags and queries.- Provide 10 suggestion in the list. Output must contain 10 SUGGESTION ELEMENTS . Here is the Travel data for you make Suggestions YOU MUST PROVIDE FULL STRUCTURE AS JSON: ".format(exampleInput, output) 
 
-def call_gpt_api(prompt):
-    endpoint = "https://api.openai.com/v1/chat/completions"
-    api_key = "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"  # Replace this with your OpenAI API key
+
+endpoint = "https://api.openai.com/v1/chat/completions"
+api_key = ""
+
+current_file_path = os.path.abspath(__file__) 
+directory_path = os.path.dirname(current_file_path) 
+
+with open(os.path.join(directory_path, "config.json") , 'r') as f:
+    api_key = json.load(f)['gpt_api_key']
+
+def __call_gpt_api(prompt):  # Replace this with your OpenAI API key
 
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
 
@@ -47,41 +79,26 @@ def call_gpt_api(prompt):
         print(f"Failed with status code {response.status_code}")
         return ""
 
+def Get_google_search_tags(TravelData,PersonalPref = None, save=False):  
 
-import os
+    PROMPT = PROMPT + json.dumps(TravelData) 
+    if PersonalPref != None:
+        PROMPT = PROMPT + json.dumps(PersonalPref) 
+    generated_text = __call_gpt_api(PROMPT) 
+    jsonData={}
+    try:
+        jsonData = json.loads(generated_text)
+    except json.decoder.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}") 
 
-# Get the absolute path of the current file
-current_file_path = os.path.abspath(__file__)
-
-# Get the directory path of the current file
-directory_path = os.path.dirname(current_file_path)
-
-# Construct the absolute path of the prompt file
-prompt_file_path = os.path.join(directory_path, "pr.txt")
-
-# Read the prompt from the file
-with open(prompt_file_path, "r") as file:
-    prompt = file.read()
-
-# Append the traveldata to the prompt
-prompt = prompt + json.dumps(traveldata)
-
-# Call the GPT API
-generated_text = call_gpt_api(prompt)
-
-# Construct the absolute path of the generated text file
-generated_text_file_path = os.path.join(directory_path, "generated_text.json")
-
-try:
-    jsonData = json.loads(generated_text)
-    # Continue with the rest of your code that uses jsonData
-except json.decoder.JSONDecodeError as e:
-    print(f"Error decoding JSON: {e}")
-
-    # Handle the error appropriately
-
-# Save the generated text to a file as JSON
-with open(generated_text_file_path, "w") as file:
-    file.write(json.dumps(jsonData))
-
-print(len(jsonData["suggestions"]))
+    if save:
+        try:
+            generated_text_file_path = os.path.join(directory_path, "generated_text.json") 
+            with open(generated_text_file_path, "w") as file:
+                file.write(json.dumps(jsonData)) 
+            print(len(jsonData["suggestions"])) 
+        except json.decoder.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}") 
+    print(jsonData)
+    return jsonData
+ 
--- api/scrapper.py
+++ api/scrapper.py
@@ -8,6 +8,15 @@ from selenium.webdriver.common.keys import Keys
 import json
 import time
 import re
+import multiprocessing 
+
+
+start = time.time()
+json = [
+    {'inputLoc' : 'The Hunger Ankara', 'inputPlaceId' : 'ChIJczooWulP0xQRqKZM1YQEWw0', 'maxReviews' : 100},
+    {'inputLoc' : 'Mama\'s Burger Dikmen', 'inputPlaceId' : 'ChIJ83EOeClF0xQRpmgaAmrCMqA', 'maxReviews' : 100}, 
+]
+processes = []
 
 def clean_string(input_string):
     # Define regex pattern to match punctuation and special characters
@@ -17,9 +26,90 @@ def clean_string(input_string):
     clean_string = re.sub(pattern, ' ', input_string)
     
     return clean_string
+
+def get_all_reviews(json):
+    for data in json:
+        processes.append(multiprocessing.Process(target=_get_reviews(data['inputLoc'], data['inputPlaceId'], data['maxReviews'], False))) 
+    for process in processes:
+        process.start() 
  
-# record start time
-start = time.time()
+ 
+def _get_reviews(inputLoc, inputPlaceId, maxReviews, sendRequest = True):
+    start = time.time()
+    URL = "https://www.google.com/maps/search/?api=1" 
+
+    if(inputLoc  != ""):
+        URL = URL +("&query={}".format(inputLoc))
+    if(inputPlaceId != ""):
+        URL = URL + ("&query_place_id={}".format(inputPlaceId)) 
+
+    driver = webdriver.Firefox()
+    driver.get(URL)
+
+    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div/div/button[2]/div[2]/div[2]") )).click()
+ 
+
+    try:
+        info = "/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[3]/div[2]/span/div[1]/button/span[1]/img" 
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, info) )).click()
+        comments =  "/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[11]/div"
+        reviews = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments) ))
+        reviewsCount = len(reviews) 
+        ActionChains(driver).key_down(Keys.END).perform()   
+        WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments+"[{}]".format(reviewsCount+1)) )) 
+        reviews = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments) ))
+    
+
+        while(len(reviews) > reviewsCount and len(reviews) < maxReviews*3): 
+            reviewsCount = len(reviews)
+            ActionChains(driver).key_down(Keys.END).perform()    
+            WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments+"[{}]".format(reviewsCount+1)) ))
+            reviews=WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments) ))
+            print(len(reviews))
+    except Exception as e:
+        print(e)
+    i = 0
+    while i < len(reviews):
+        if reviews[i].get_attribute("class") == "qCHGyb":
+            del reviews[i]
+        else:
+            i += 1
+
+    reviewsCount = len(reviews)
+    #vzX5Ic
+    rewievsResult = []
+    
+    for review in reviews:
+        try:
+            upper = review.find_elements(By.XPATH,"div/div/div")
+            ind = len(upper)-1
+            if(ind < 0):
+                continue
+            rating = upper[ind].find_elements(By.XPATH, "div/span/span")  
+            rate = len([elem for elem in rating if elem.get_attribute("class") == "hCCjke vzX5Ic google-symbols NhBTye"]) 
+            textandmore = upper[ind].find_elements(By.XPATH,"div[2]/div/span")
+            if len(textandmore) > 1:
+                textandmore[1].click()
+                textandmore = WebDriverWait(upper[ind],10).until(EC.presence_of_all_elements_located((By.XPATH,"div[2]/div/span"))) 
+
+            rewievsResult.append({"place": inputLoc ,"rating":rate,"review":clean_string(textandmore[0].text)})
+        except Exception as e:
+            print(e)
+    # record end time
+    end = time.time()
+    # print the difference between start
+    # and end time in milli. secs
+    print("The time of execution of above program is :",(end-start), "seconds.")
+    # Convert rewievsResult to JSON
+    if sendRequest:
+        json_data = json.dumps(rewievsResult, ensure_ascii=False)
+        
+        # Write JSON data to a file with utf-8 encoding
+        with open('reviews.json', 'a', encoding='utf-8') as file:
+            file.write(json_data)
+        return rewievsResult
+
+"""
 
 inputLoc = 'The Hunger Ankara'#"Mama's Burger Dikmen"
 inputPlaceId = 'ChIJczooWulP0xQRqKZM1YQEWw0'#'ChIJ83EOeClF0xQRpmgaAmrCMqA' 
@@ -98,4 +188,4 @@ json_data = json.dumps(rewievsResult, ensure_ascii=False)
 with open('reviews.json', 'w', encoding='utf-8') as file:
     file.write(json_data)
 
- 
+ """
