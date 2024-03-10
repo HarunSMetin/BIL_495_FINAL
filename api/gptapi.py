dffeffb033cc4938cfed999f70c887d0626dc5e6
Harun Serkan Metin <harun.serkan.metin@gmail.com>
2024-03-11 01:05:40 +0300

    Major-Update : Automated comment scrapping from google maps places based on travel details;

--- api/gptapi.py
+++ api/gptapi.py
@@ -1,104 +1,84 @@
 import requests
 import json
 import os
+class gpt_api:
+    def __init__(self, API_KEY=None): 
+            exampleInput = {
+            "departureDate": "2024-01-03 00:00:00.000",
+            "returnDate": "2024-01-04 00:00:00.000",
+            "specialComment": "I love eating Pizza. Explore new places and see lots of places",
+            "departureLocation": "Ankara",
+            "desiredDestination": "Istanbul, Söğütlüçeşme",
+            "travelTransportation": "Car",
+            "purposeOfVisit": "Cultural experiences and sightseeing",
+            "estimatedBudget": 5,
+            "accommodationPreferences": "Mid-range",
+            "activitiesPreferences": "Relaxation and wellness (e.g., spas, beaches)",
+            "dietaryRestrictions": "No restrictions",
+            "travelingWithOthers": "Small group (3-5 people)",
+            "localRecommendations": "Yes"
+            }
 
-exampleInput = {
-    "departureDate": "2024-01-03 00:00:00.000",
-    "returnDate": "2024-01-04 00:00:00.000",
-    "specialComment": "I love eating Pizza. Explore new places and see lots of places",
-    "departureLocation": "Ankara",
-    "desiredDestination": "Istanbul, Söğütlüçeşme",
-    "travelTransportation": "Car",
-    "purposeOfVisit": "Cultural experiences and sightseeing",
-    "estimatedBudget": 5,
-    "accommodationPreferences": "Mid-range",
-    "activitiesPreferences": "Relaxation and wellness (e.g., spas, beaches)",
-    "dietaryRestrictions": "No restrictions",
-    "travelingWithOthers": "Small group (3-5 people)","localRecommendations": "Yes"
-    }
+            output = {
+                "suggestions": [
+                    {"place_types": 
+                        {"food_and_drink_based": ["pizza_restaurant"],"entertainment_and_recreation_center": ["tourist_attraction"]},"fields": {"atmospheric_data_fields": ["user_ratings_total"]},"query": "Pizza Restaurants in Istanbul, Söğütlüçeşme"},{"place_types": {"culture_based": ["art_gallery", "museum", "performing_arts_theater"],"entertainment_and_recreation_center": ["amusement_center", "historical_landmark", "tourist_attraction"]},"query": "Historical places in Istanbul"},
+                    ]
+                }
 
-output = {
-    "suggestions": [
-        {"place_types": 
-            {"food_and_drink_based": ["pizza_restaurant"],"entertainment_and_recreation_center": ["tourist_attraction"]},"fields": {"atmospheric_data_fields": ["user_ratings_total"]},"query": "Pizza Restaurants in Istanbul, Söğütlüçeşme"},{"place_types": {"culture_based": ["art_gallery", "museum", "performing_arts_theater"],"entertainment_and_recreation_center": ["amusement_center", "historical_landmark", "tourist_attraction"]},"query": "Historical places in Istanbul"},
-        ]
-    }
-
-traveldata = {
-    "id": "PsSAcQDOSL4g5b6q7O7FN",
-    "name": "Ankara - Çanakkale Travel",
-    "description": "Travel",
-    "creatorId": "TDPuYUpUqvhzzVTAZoRnpw68Ym73",
-    "isPublic": True,
-    "isCompleted": True,
-    "lastUpdate": "2024-02-08 00:00:00.000",
-    "members": ["TDPuYUpUqvhzzVTAZoRnpw68Ym73"],
-    "departureDate": "2024-02-08 00:00:00.000",
-    "returnDate": "2024-02-15 00:00:00.000",
-    "departureLocation": "Ankara",
-    "desiredDestination": "Çanakkale",
-    "travelTransportation": "Bus",
-    "purposeOfVisit": "Cultural experiences and sightseeing",
-    "estimatedBudget": 5000,
-    "accommodationPreferences": "Mid-range",
-    "activitiesPreferences": "Relaxation and wellness (e.g., spas, beaches)",
-    "dietaryRestrictions": "No restrictions",
-    "travelingWithOthers": "Just Me",
-    "specialComment": "I am a student and I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local food and I want to try the local desserts. I want to visit the historical places in Çanakkale. I want to stay in a hotel with a sea view. I want to visit the beaches and the historical places in Çanakkale. I want to eat local",
-    "localRecommendations": "Yes",
-    "lastUpdatedQuestionId": "12_LocalRecommendations",
-}
-
-PROMPT = "#Description:#You're developing an application to help users plan customized travel itineraries using the Google Maps API. Your task is to process structured JSON input, representing a user's travel details and preferences, and generate a comprehensive list of suggested places to visit and activities to engage in during the specified travel period.#Input:#The input comprises a JSON object with the following fields:- `departureDate`: Date of departure.- `returnDate`: Date of return.- `specialComment`: Additional comments or preferences provided by the user.- Additional fields for specific preferences or constraints.#Example Input:#```json{}```#Process:#1. Calculate the travel period duration from `departureDate` to `returnDate`.2. Consider `specialComment` to understand user preferences.3. Generate suggestions proportional to the travel duration, ensuring 10 suggestions in the list.4. Select destinations based on predefined tags like parks, restaurants, etc.5. Customize suggestions with relevant tags and query strings.#Output:#A JSON object with a curated list of suggestions, each containing:- Tags categorizing the suggested place/activity.- Query strings for Google Maps API.#Example Output:#{}#Constraints:#- Ensure diverse suggestions catering to various interests.- Output adheres to specified structure with relevant tags and queries.- Provide 10 suggestion in the list. Output must contain 10 SUGGESTION ELEMENTS . Here is the Travel data for you make Suggestions YOU MUST PROVIDE FULL STRUCTURE AS JSON: ".format(exampleInput, output) 
+        
+            self.PROMPT = "#Description:#You're developing an application to help users plan customized travel itineraries using the Google Maps API. Your task is to process structured JSON input, representing a user's travel details and preferences, and generate a comprehensive list of suggested places to visit and activities to engage in during the specified travel period.#Input:#The input comprises a JSON object with the following fields:- `departureDate`: Date of departure.- `returnDate`: Date of return.- `specialComment`: Additional comments or preferences provided by the user.- Additional fields for specific preferences or constraints.#Example Input:#```json{}```#Process:# Calculate the travel period duration from `departureDate` to `returnDate`. Consider `specialComment` to understand user preferences. Generate suggestions proportional to the travel duration, ensuring 7 suggestions in the list. Select destinations based on predefined tags like parks, restaurants, etc. Customize suggestions with relevant tags and query strings.#Output:# A JSON object with a curated list of suggestions, each containing: Tags categorizing the suggested place/activity. Query strings for Google Maps API.#Example Output:# {} #Constraints:# Ensure diverse suggestions catering to various interests. Output adheres to specified structure with relevant tags and queries. Provide 7 suggestion in the list. Output must contain 7 SUGGESTION ELEMENTS . Here is the Travel data for you make Suggestions YOU MUST PROVIDE FULL STRUCTURE AS JSON: ".format(exampleInput, output) 
 
 
-endpoint = "https://api.openai.com/v1/chat/completions"
-api_key = ""
+            self.endpoint = "https://api.openai.com/v1/chat/completions"
+            self.api_key = ""
 
-current_file_path = os.path.abspath(__file__) 
-directory_path = os.path.dirname(current_file_path) 
+            self.current_file_path = os.path.abspath(__file__) 
+            self.directory_path = os.path.dirname(self.current_file_path) 
 
-with open(os.path.join(directory_path, "config.json") , 'r') as f:
-    api_key = json.load(f)['gpt_api_key']
+            with open(os.path.join(self.directory_path, "config.json") , 'r') as f:
+                 self.api_key = json.load(f)['gpt_api_key']
+    
 
-def __call_gpt_api(prompt):  # Replace this with your OpenAI API key
 
-    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+    def __call_gpt_api(self,prompt):  # Replace this with your OpenAI API key
 
-    data = {
-        "model": "gpt-3.5-turbo",  # Adjust the model name as per your preference
-        "messages": [{"role": "user", "content": prompt}],
-        "temperature": 0.3,  # Adjust the temperature as per your preference
-    }
+        headers = {"Content-Type": "application/json", "Authorization": f"Bearer {self.api_key}"}
 
-    response = requests.post(endpoint, headers=headers, json=data)
-    if response.status_code == 200:
-        print(response.json()["usage"])
-        return response.json()["choices"][0]["message"]["content"]
-    else:
-        print(f"Failed with status code {response.status_code}")
-        return ""
+        data = {
+            "model": "gpt-3.5-turbo",  # Adjust the model name as per your preference
+            "messages": [{"role": "user", "content": prompt}],
+            "temperature": 0.3,  # Adjust the temperature as per your preference
+        }
 
-def Get_google_search_tags(TravelData,PersonalPref = None, save=False):  
+        response = requests.post(self.endpoint, headers=headers, json=data)
+        if response.status_code == 200:
+            print(response.json()["usage"])
+            return response.json()["choices"][0]["message"]["content"]
+        else:
+            print(f"Failed with status code {response.status_code}")
+            return ""
 
-    PROMPT = PROMPT + json.dumps(TravelData) 
-    if PersonalPref != None:
-        PROMPT = PROMPT + json.dumps(PersonalPref) 
-    generated_text = __call_gpt_api(PROMPT) 
-    jsonData={}
-    try:
-        jsonData = json.loads(generated_text)
-    except json.decoder.JSONDecodeError as e:
-        print(f"Error decoding JSON: {e}") 
+    def Get_google_search_tags(self,TravelData,PersonalPref = None, save=False):  
 
-    if save:
+        self.PROMPT = self.PROMPT + json.dumps(TravelData, indent=4, sort_keys=True, default=str)
+        if PersonalPref != None:
+            self.PROMPT = self.PROMPT + json.dumps(PersonalPref,  indent=4, sort_keys=True, default=str)
+        generated_text = self.__call_gpt_api(self.PROMPT) 
+        jsonData={}
         try:
-            generated_text_file_path = os.path.join(directory_path, "generated_text.json") 
-            with open(generated_text_file_path, "w") as file:
-                file.write(json.dumps(jsonData)) 
-            print(len(jsonData["suggestions"])) 
+            jsonData = json.loads(generated_text)
         except json.decoder.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}") 
-    print(jsonData)
-    return jsonData
- 
+
+        if save:
+            try:
+                generated_text_file_path = os.path.join(self.directory_path, "generated_text.json") 
+                with open(generated_text_file_path, "w") as file:
+                    file.write(json.dumps(jsonData)) 
+                print(len(jsonData["suggestions"])) 
+            except json.decoder.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}") 
+        print(jsonData)
+        return jsonData
+    
--- api/scrapper.py
+++ api/scrapper.py
@@ -8,15 +8,16 @@ from selenium.webdriver.common.keys import Keys
 import json
 import time
 import re
-import multiprocessing 
-
+from multiprocessing import Process, Manager
+import requests 
 
 start = time.time()
 json = [
     {'inputLoc' : 'The Hunger Ankara', 'inputPlaceId' : 'ChIJczooWulP0xQRqKZM1YQEWw0', 'maxReviews' : 100},
     {'inputLoc' : 'Mama\'s Burger Dikmen', 'inputPlaceId' : 'ChIJ83EOeClF0xQRpmgaAmrCMqA', 'maxReviews' : 100}, 
 ]
-processes = []
+
+rewievsResult = []
 
 def clean_string(input_string):
     # Define regex pattern to match punctuation and special characters
@@ -27,19 +28,29 @@ def clean_string(input_string):
     
     return clean_string
 
-def get_all_reviews(json):
-    for data in json:
-        processes.append(multiprocessing.Process(target=_get_reviews(data['inputLoc'], data['inputPlaceId'], data['maxReviews'], False))) 
-    for process in processes:
-        process.start() 
+def get_all_reviews(json):  
+    rewievsResult.clear()
+    with Manager() as manager:
+        lock = manager.Lock() 
+        for J in json: 
+            processes = []
+            for data in J:
+                processes.append(Process(target=_get_reviews, args = (data['name'], data['place_id'],lock))) 
+            for process in processes:
+                process.start() 
+            for process in processes:
+                process.join()   
+                print("Length Of rewievsResult {} ".format(len(rewievsResult)))
+
+    return rewievsResult
  
  
-def _get_reviews(inputLoc, inputPlaceId, maxReviews, sendRequest = True):
-    start = time.time()
+ 
+def _get_reviews(inputLoc, inputPlaceId,lock ,maxReviews=100, save = False): 
     URL = "https://www.google.com/maps/search/?api=1" 
 
     if(inputLoc  != ""):
-        URL = URL +("&query={}".format(inputLoc))
+        URL = URL +("&query={}".format(requests.utils.quote(inputLoc)))
     if(inputPlaceId != ""):
         URL = URL + ("&query_place_id={}".format(inputPlaceId)) 
 
@@ -65,7 +76,6 @@ def _get_reviews(inputLoc, inputPlaceId, maxReviews, sendRequest = True):
             ActionChains(driver).key_down(Keys.END).perform()    
             WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments+"[{}]".format(reviewsCount+1)) ))
             reviews=WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments) ))
-            print(len(reviews))
     except Exception as e:
         print(e)
     i = 0
@@ -77,7 +87,6 @@ def _get_reviews(inputLoc, inputPlaceId, maxReviews, sendRequest = True):
 
     reviewsCount = len(reviews)
     #vzX5Ic
-    rewievsResult = []
     
     for review in reviews:
         try:
@@ -91,101 +100,19 @@ def _get_reviews(inputLoc, inputPlaceId, maxReviews, sendRequest = True):
             if len(textandmore) > 1:
                 textandmore[1].click()
                 textandmore = WebDriverWait(upper[ind],10).until(EC.presence_of_all_elements_located((By.XPATH,"div[2]/div/span"))) 
+            with lock:
+                rewievsResult.append({"place": inputLoc ,"rating":rate,"review":clean_string(textandmore[0].text)})
 
-            rewievsResult.append({"place": inputLoc ,"rating":rate,"review":clean_string(textandmore[0].text)})
         except Exception as e:
-            print(e)
-    # record end time
-    end = time.time()
-    # print the difference between start
-    # and end time in milli. secs
-    print("The time of execution of above program is :",(end-start), "seconds.")
+            print(e) 
     # Convert rewievsResult to JSON
-    if sendRequest:
+    if save:
         json_data = json.dumps(rewievsResult, ensure_ascii=False)
         
         # Write JSON data to a file with utf-8 encoding
         with open('reviews.json', 'a', encoding='utf-8') as file:
             file.write(json_data)
-        return rewievsResult
-
-"""
-
-inputLoc = 'The Hunger Ankara'#"Mama's Burger Dikmen"
-inputPlaceId = 'ChIJczooWulP0xQRqKZM1YQEWw0'#'ChIJ83EOeClF0xQRpmgaAmrCMqA' 
-maxReviews = 1000
-
-
-URL = "https://www.google.com/maps/search/?api=1" 
-
-if(inputLoc  != ""):
-    URL = URL +("&query={}".format(inputLoc))
-if(inputPlaceId != ""):
-    URL = URL + ("&query_place_id={}".format(inputPlaceId)) 
-
-driver = webdriver.Firefox()
-driver.get(URL)
-
-WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[3]/div/div/button[2]/div[2]/div[2]") )).click()
- 
-
-try:
-    info = "/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[3]/div[2]/span/div[1]/button/span[1]/img" 
-    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.XPATH, info) )).click()
-    comments =  "/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[11]/div"
-    reviews = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments) ))
-    reviewsCount = len(reviews) 
-    ActionChains(driver).key_down(Keys.END).perform()   
-    WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments+"[{}]".format(reviewsCount+1)) )) 
-    reviews = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments) ))
     
+    driver.close()
+    return rewievsResult
 
-    while(len(reviews) > reviewsCount and len(reviews) < maxReviews*3): 
-        reviewsCount = len(reviews)
-        ActionChains(driver).key_down(Keys.END).perform()    
-        WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments+"[{}]".format(reviewsCount+1)) ))
-        reviews=WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.XPATH,comments) ))
-        print(len(reviews))
-except Exception as e:
-    print(e) 
-
-i = 0
-while i < len(reviews):
-    if reviews[i].get_attribute("class") == "qCHGyb":
-        del reviews[i]
-    else:
-        i += 1
-
-reviewsCount = len(reviews)
-#vzX5Ic 
-rewievsResult = []
-for review in reviews:
-    try:
-        upper = review.find_elements(By.XPATH,"div/div/div")
-        ind = len(upper)-1
-        if(ind < 0):
-            continue
-        rating = upper[ind].find_elements(By.XPATH, "div/span/span")  
-        rate = len([elem for elem in rating if elem.get_attribute("class") == "hCCjke vzX5Ic google-symbols NhBTye"]) 
-        textandmore = upper[ind].find_elements(By.XPATH,"div[2]/div/span")
-        if len(textandmore) > 1:
-            textandmore[1].click()
-            textandmore = WebDriverWait(upper[ind],10).until(EC.presence_of_all_elements_located((By.XPATH,"div[2]/div/span"))) 
-
-        rewievsResult.append({"place": inputLoc ,"rating":rate,"review":clean_string(textandmore[0].text)})
-    except Exception as e:
-        print(e)  
-# record end time
-end = time.time()
- 
-# print the difference between start 
-# and end time in milli. secs
-print("The time of execution of above program is :",(end-start), "seconds.")
-# Convert rewievsResult to JSON
-json_data = json.dumps(rewievsResult, ensure_ascii=False)
-
-# Write JSON data to a file with utf-8 encoding
-with open('reviews.json', 'w', encoding='utf-8') as file:
-    file.write(json_data)
-
- """
